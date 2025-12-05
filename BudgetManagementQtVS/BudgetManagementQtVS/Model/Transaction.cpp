b7d34eeed797b8ffc1c792a65f9fda84e8ab48cb
Chell0 <frydjakub@gmail.com>
2025-12-05 14:15:19 +0100

    added Transaction Model

---
+++ BudgetManagementQtVS/BudgetManagementQtVS/Model/Transaction.cpp
@@ -0,0 +1,83 @@
+#include "Model/Transaction.h"
+
+Transaction::Transaction(int TransactionId, QString transactionName, QDate transactionDate, QString transactionDescription, double transactionAmount, TransactionType transactionType)
+{
+	this->transactionId = TransactionId;
+	this->transactionName = transactionName;
+	this->transactionDate = transactionDate;
+	this->transactionDescription = transactionDescription;
+	this->transactionAmount = transactionAmount;
+	this->transactionType = transactionType;
+}
+
+Transaction::~Transaction() {}
+
+
+//getters
+
+int Transaction::getTransactionId() const
+{
+	return transactionId;
+}
+
+QString Transaction::getTransactionName() const
+{
+	return transactionName;
+}
+
+QDate Transaction::getTransactionDate() const
+{
+	return transactionDate;
+}
+
+QString Transaction::getTransactionDescription() const
+{
+	return transactionDescription;
+}
+double Transaction::getTransactionAmount() const
+{
+	return transactionAmount;
+}
+
+//setters
+
+void Transaction::setTransactionId(int id)
+{
+	transactionId = id;
+}
+
+void Transaction::setTransactionName(const QString& name)
+{
+	transactionName = name;
+}	
+
+void Transaction::setTransactionDate(const QDate& date)
+{
+	transactionDate = date;
+}
+
+void Transaction::setTransactionDescription(const QString& description)
+{
+	transactionDescription = description;
+}
+
+void Transaction::setTransactionAmount(double amount)
+{
+	transactionAmount = amount;
+}
+
+void Transaction::setTransactionType(TransactionType type)
+{
+	transactionType = type;
+}
+
+void Transaction::editTransaction(const QString& name, const QDate& date, const QString& description, double amount, TransactionType type)
+{
+	setTransactionName(name);
+	setTransactionDate(date);
+	setTransactionDescription(description);
+	setTransactionAmount(amount);
+	setTransactionType(type);
+}
+
+
