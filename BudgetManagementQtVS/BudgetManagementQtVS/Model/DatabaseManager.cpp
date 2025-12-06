28fb8f15d1145c6c7da1b64c99a8c82c63709cb1
Greexoff <greexoff@gmail.com>
2025-12-06 14:50:14 +0100

    databaseChanges

--- BudgetManagementQtVS/BudgetManagementQtVS/Model/DatabaseManager.cpp
+++ BudgetManagementQtVS/BudgetManagementQtVS/Model/DatabaseManager.cpp
@@ -14,33 +14,38 @@ DatabaseManager::DatabaseManager() {
         qDebug() << "Database opened correctly";
     }
 
+
     QSqlQuery tableCreationQuery;
     tableCreationQuery.exec("PRAGMA foreign_keys = ON;");//Allowing to create foreign keys
     //Creating table for users
     tableCreationQuery.exec("CREATE TABLE IF NOT EXISTS users"
         "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        "username TEXT UNIQUE NOT NULL,"
-        "password TEXT NOT NULL)");
+        "username TEXT UNIQUE NOT NULL CHECK (username != ''),"
+        "password TEXT NOT NULL CHECK (password != ''))");
     //Creating table for profiles
     tableCreationQuery.exec("CREATE TABLE IF NOT EXISTS profiles"
         "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        "profile_name TEXT UNIQUE NOT NULL, "
+        "profile_name TEXT UNIQUE NOT NULL CHECK (profile_name != ''), "
         "user_id INTEGER, "
         "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE)");
+    //Creating table for categories
+    tableCreationQuery.exec("CREATE TABLE IF NOT EXISTS category"
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "category_name TEXT NOT NULL CHECK (category_name != ''))");
+    tableCreationQuery.exec("INSERT OR IGNORE INTO category (id, category_name) VALUES (1, 'Brak Kategorii')");
     //Creating table for transactions
     tableCreationQuery.exec("CREATE TABLE IF NOT EXISTS transactions"
         "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        "name TEXT NOT NULL, "
-        "type TEXT NOT NULL, "
-        "date TEXT NOT NULL, "
+        "name TEXT NOT NULL CHECK (name != ''), "
+        "type TEXT NOT NULL CHECK (type != ''), "
+        "date TEXT NOT NULL CHECK (date != ''), "
         "description TEXT, "
         "amount REAL NOT NULL, "
-        "category_id INTEGER DEFAULT 0, "//tutaj trzeba bedzie pewnie dorobic kolejny foreign key w ktoras strone   
-        "profile_id INTEGER NOT NULL, "
-        "FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE"
+        "category_id INTEGER DEFAULT 1, "
+        "profile_id INTEGER, "
+        "FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE, "
+        "FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET DEFAULT"
         ")");
-    //IN PROGRESS Creating table for category
-    //IN PROGRESS Here make sql query for creating table for category with id, name, 
 
     QSqlQuery adminQuery(datebaseInstance);
     adminQuery.prepare("SELECT COUNT(*) FROM users WHERE username = 'admin'");
